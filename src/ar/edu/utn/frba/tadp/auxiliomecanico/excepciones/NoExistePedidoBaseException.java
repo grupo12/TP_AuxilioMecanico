4eec86aca4476a3016ce648ffb37ff877c09723b
avila--07 <avila--07@hotmail.com>
2010-10-16 18:37:11 -0300

    .

---
+++ src/ar/edu/utn/frba/tadp/auxiliomecanico/excepciones/NoExistePedidoBaseException.java
@@ -0,0 +1,19 @@
+package ar.edu.utn.frba.tadp.auxiliomecanico.excepciones;
+
+import ar.edu.utn.frba.tadp.auxiliomecanico.pedidos.Pedido;
+
+public class NoExistePedidoBaseException extends UserException {
+
+	private static final long serialVersionUID = 1L;
+
+	private Pedido pedido;
+
+	public NoExistePedidoBaseException(String string, Pedido pedido) {
+		super(string);
+		this.pedido = pedido;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+}
